64788da6a3f39b6d4d9c2465cc535ad97dde3e6d
stenman.desktop <roger.stenman@gmail.com>
2013-06-02 19:07:07 +0200

    -Fixed a typo in  Spring-Module.xml causing AspectJ to not work properly -Implemented a simple advice

--- src/main/java/com/example/samplewebapp/aspect/logging/LoggingAspect.java
+++ src/main/java/com/example/samplewebapp/aspect/logging/LoggingAspect.java
@@ -3,16 +3,18 @@ package com.example.samplewebapp.aspect.logging;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class LoggingAspect {
 
-	@Before("execution(* com.hm.online.accountingmanager.domain.service.AccountingManagementServiceImpl.fetchTransactionTypeCount(..))")
+	@Before("execution(* fetch*())")
 	public void logBefore(JoinPoint joinPoint) {
 
-		System.out.println("******");
+		System.out.println("****************************************************************************************");
 		System.out.println("logBefore() is running!");
 		System.out.println("hijacked : " + joinPoint.getSignature().getName());
-		System.out.println("******");
+		System.out.println("****************************************************************************************");
 	}
 }
